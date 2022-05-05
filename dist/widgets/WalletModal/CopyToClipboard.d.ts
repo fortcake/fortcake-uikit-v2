f24a6e015a44a9f45b168d37b8b36bce9cdc474b
Fortcake <forrtcake@gmail.com>
2022-05-06 00:44:02 +0800

    Swap UI update

--- dist/widgets/WalletModal/CopyToClipboard.d.ts
+++ dist/widgets/WalletModal/CopyToClipboard.d.ts
@@ -2,5 +2,5 @@ import React from "react";
 interface Props {
     toCopy: string;
 }
-declare const CopyToClipboard: React.FC<Props>;
+export declare const CopyToClipboard: React.FC<Props>;
 export default CopyToClipboard;
--- src/widgets/WalletModal/index.tsx
+++ src/widgets/WalletModal/index.tsx
@@ -1,4 +1,5 @@
 export { default as useWalletModal } from "./useWalletModal";
 export { connectorLocalStorageKey } from "./config";
 export { ConnectorNames } from "./types";
+export { CopyToClipboard } from "./CopyToClipboard";
 export type { Login } from "./types";
